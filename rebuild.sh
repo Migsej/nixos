f0ad8ed07ca87ac0913c23af8f78d9251a46cd91
migsej <vincentkbonne@gmail.com>
2024-10-07 15:33:59 +0200

    man  7 okt 15:33:59 CEST 2024

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 sudo nixos-rebuild switch
-git -C ~/nixos add .
-git -C ~/nixos commit -m "$(date)"
-git -C ~/nixos push -u origin main
+
+echo "Do you Want to commit this change (Y/N"
+
+read -r response
+
+if [[ "$response" == "Y" ]]; then
+  git -C ~/nixos add .
+  git -C ~/nixos commit -m "$(date)"
+  git -C ~/nixos push -u origin main
+fi
+
