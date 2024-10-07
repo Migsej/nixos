f3dc56eb3bdba5283dcd2cd11bc5d2dfd8d2a9cf
migsej <vincentkbonne@gmail.com>
2024-10-07 15:28:24 +0200

    man  7 okt 15:28:24 CEST 2024

--- home.nix
+++ home.nix
@@ -10,19 +10,12 @@
       	pycryptodome
       	ipython
     	]);
-      tex = (pkgs.texlive.combine {
-        inherit (pkgs.texlive) scheme-basic
-        #dvisvgm dvipng # for preview and export as html
-        etoolbox bibtex wrapfig amsmath ulem hyperref capt-of;
-        #(setq org-latex-compiler "lualatex")
-        #(setq org-preview-latex-default-process 'dvisvgm)
-      });
     	school = with pkgs; [
         kicad
         obsidian
         libreoffice
         zathura
-        tex
+        tetex
       ];
     	in with pkgs; [
         discord
